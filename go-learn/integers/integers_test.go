8b6c757e4205f45e886d5d062cb700d68a772370
chore1110 <yosuke-nishimura@c-fo.com>
2024-02-07 14:35:35 +0900

    https://andmorefine.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#nitesutowoku-2

---
+++ go-learn/integers/integers_test.go
@@ -0,0 +1,36 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected '%s' but got '%q'", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
---
+++ go-learn/main/sum.go
@@ -0,0 +1,17 @@
+package main
+
+func Sum1(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
